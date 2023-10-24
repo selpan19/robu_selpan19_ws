0578a964c29d1aa2e8de6a71a67d7bce3aef448f
selpan19 <selpan19@htl-kaindorf.at>
2023-10-24 12:07:22 +0200

    24.10.2023/1

--- src/robu/robu/ex10_wallfollower.py
+++ src/robu/robu/ex10_wallfollower.py
@@ -1,6 +1,25 @@
 import math
+import rclpy
 
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from std_msgs.msg import LaserScan
 
+from rclpy.qos import qos_profile_sensor_data
+
+import numpy as np
+
+from enum import IntEnum
+
+class WallfollowerStates(IntEnum):
+    WF_STATE_DETECTWALL = 0,
+    WF_STATE_DRIVE2WALL = 1,
+    WF_STATE_ROTATE2WALL =2,
+    WF_STATE_FOLLOWWALL = 3
+
+class WallFollower(rclpy.Node):
+    def __init__(self):
+        super().__init__('WallFollower')
 
 def main(args=None):
     print("Hello from Wallfollower!")
