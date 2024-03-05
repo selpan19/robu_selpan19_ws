73acfa5b299172afeaec302feb397faab1a31505
selpan19 <you@example.com>
2024-03-05 12:20:39 +0100

    05.03.2024 12:20

--- src/robu/robu/ex12_fibonacci_server.py
+++ src/robu/robu/ex12_fibonacci_server.py
@@ -1,5 +1,6 @@
 #https://docs.ros.org/en/foxy/Tutorials/Intermediate/Writing-an-Action-Server-Client/Py.html
 
+import time
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
@@ -8,36 +9,35 @@ from rclpy.node import Node
 from robu_interfaces.action import Fibonacci
 
 
+
 class FibonacciActionServer(Node):
 
     def __init__(self):
         super().__init__('fibonacci_action_server')
-        #Actionserver erstellen
         self._action_server = ActionServer(
-            #Klasse in der der Actionsserver erstellt werden soll
             self,
-            #Schnittstelle
             Fibonacci,
-            #Bezeichnung für action
             'fibonacci',
-            #Methode, die aufgerufen wird wenn Anfrage kommt
             self.execute_callback)
 
-    #Anfragemethode anlegen (self, ziel): This is the method that will be called to execute a goal once it is accepted.
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
 
-        sequence = [0, 1]
+        feedback_msg = Fibonacci.Feedback()
+        feedback_msg.partial_sequence = [0, 1]
 
         for i in range(1, goal_handle.request.order):
-          sequence.append(sequence[i] + sequence[i-1])
+            feedback_msg.partial_sequence.append(
+                feedback_msg.partial_sequence[i] + feedback_msg.partial_sequence[i-1])
+            self.get_logger().info('Feedback: {0}'.format(feedback_msg.partial_sequence))
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(1)
 
         goal_handle.succeed()
 
         result = Fibonacci.Result()
-        result.sequence = sequence
+        result.sequence = feedback_msg.partial_sequence
         return result
-    
 
 
 def main(args=None):
@@ -45,7 +45,6 @@ def main(args=None):
 
     fibonacci_action_server = FibonacciActionServer()
 
-    #wartet bis etwas passiert
     rclpy.spin(fibonacci_action_server)
 
 
