60b69cc3c770c666d8055cd664109ef738476559
selpan19 <selpan19@htl-kaindorf.at>
2024-04-02 13:16:16 +0200

    02.04.2024 13:15

--- src/robu/launch/ex14_navigation_launch.py
+++ src/robu/launch/ex14_navigation_launch.py
@@ -27,4 +27,59 @@ def generate_launch_description():
 
     robot_name = 'turtlebot3_burger'
 
+    robot_urdf = os.path.join(nav2_bringup_dir, 'urdf', robot_name+'.urdf')     #Roboter Beschreibung File für RVIZ
+    robot_model_file = os.path.join(tb3_world_dir, 'models', robot_name+'model.sdf') #Roboter Beschreibung File für Gazebo
     
+    robot_pose = {
+        'x' : '-2.0',
+        'y' : '-0.5',
+        'z' : '0.01',
+        'R' : '0.00',
+        'P' : '0.00',
+        'Y' : '0.00'
+    }
+
+    print("world: ", world_file)
+    print("robot_urdf: ", robot_urdf)
+    print("map_yaml_file: ", map_yaml_file)
+    print("robot_model_file: ", robot_model_file)
+
+    use_rviz = LaunchConfiguration('use_rviz')
+    headless = LaunchConfiguration('headless')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_use_rviz_cmd = DeclareLaunchArgument('use_rviz', default_value=True)
+    declare_headless_cmd = DeclareLaunchArgument('headless', default_value=False)
+    declare_use_sim_time_cmd = DeclareLaunchArgument('use_sim_time', default_value=True)
+
+    start_gazebo_server_cmd = ExecuteProcess(
+        cmd=['gzserver', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_file],
+        cwd=[tb3_world_dir],
+        output='screen'
+    )
+
+    start_gazebo_client_cmd = ExecuteProcess(
+        condition = IfCondition(PythonExpression(['not', headless]))
+        cmd=['gzclient'],
+        cwd=[tb3_world_dir],
+        output='screen'
+    )
+
+
+    #Roboter in Gazebo plazieren
+    start_gazebo_spawner_cmd =  Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        output='screen',
+        arguments=['-entity', robot_name
+        '-file', robot_model_file,
+        '-x', robot_pose['x'], '-y', robot_pose['y'], '-z', robot_pose['z'], 
+        '-R', robot_pose['R'], '-P', robot_pose['P'], '-Y', robot_pose['Y']
+        ]
+    )
+
+    #Beschreibung des Roboters für RVIZ einlesen
+    with open(robot_urdf, 'r') as infp:
+        robot_descr = infp.read()
+
+
